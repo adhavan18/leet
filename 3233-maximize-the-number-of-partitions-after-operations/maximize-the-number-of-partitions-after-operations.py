1fcbc982e41b63a631b81458747051a25e1add40
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2025-10-18 05:26:32 +0530

    Time: 1417 ms (13.79%) | Memory: 256.9 MB (39.66%) - LeetSync

---
+++ 3233-maximize-the-number-of-partitions-after-operations/maximize-the-number-of-partitions-after-operations.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxPartitionsAfterOperations(self, s: str, k: int) -> int:
+        from functools import lru_cache
+
+        def bits(x):
+            return bin(x).count("1")
+
+        n = len(s)
+        arr = [ord(c) - 97 for c in s]
+
+        @lru_cache(None)
+        def dfs(i, mask, changed):
+            if i == n:
+                return 1 if mask else 0
+
+            res = 0
+            c = arr[i]
+            new_mask = mask | (1 << c)
+
+            # If adding this char exceeds k distinct, start new partition
+            if bits(new_mask) > k:
+                res = max(res, 1 + dfs(i + 1, 1 << c, changed))
+            else:
+                res = max(res, dfs(i + 1, new_mask, changed))
+
+            # Try changing this character (only once)
+            if not changed:
+                for ch in range(26):
+                    if ch == c:
+                        continue
+                    new_mask2 = mask | (1 << ch)
+                    if bits(new_mask2) > k:
+                        res = max(res, 1 + dfs(i + 1, 1 << ch, True))
+                    else:
+                        res = max(res, dfs(i + 1, new_mask2, True))
+
+            return res
+
+        return dfs(0, 0, False)
