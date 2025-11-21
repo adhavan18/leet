c3ba667e3a3aa38a9243de32c7d15508f052c7e1
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2025-11-21 10:50:19 +0530

    Time: 74 ms (98.98%) | Memory: 18.1 MB (82.86%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        count = 0
+        
+        for c in "abcdefghijklmnopqrstuvwxyz":
+            if c not in s:
+                continue
+            
+            first = s.index(c)
+            last = s.rindex(c)
+            
+            if first == last:
+                continue
+            
+            middle = set(s[first+1:last])
+            count += len(middle)
+        
+        return count
