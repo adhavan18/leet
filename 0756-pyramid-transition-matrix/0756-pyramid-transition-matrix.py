ba03cf48a58c0b4539d4f19900d9bb77f88757f8
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2025-12-29 21:27:13 +0530

    Time: 8683 ms (5.45%), Space: 17.6 MB (95.05%) - LeetHub

---
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def pyramidTransition(self, bottom: str, allowed: List[str]) -> bool:
+        from collections import defaultdict
+        from functools import lru_cache 
+        map = defaultdict(list)
+        for a, b, c in allowed:
+            map[a+b].append(c)
+        def dfs(row: str) -> bool:
+            if len(row) == 1:
+                return True
+
+            def backtrack(i, next_row):
+                if i == len(row) - 1:
+                    return dfs(next_row)
+
+                pair = row[i:i+2]
+                if pair not in map:
+                    return False
+
+                for ch in map[pair]:
+                    if backtrack(i + 1, next_row + ch):
+                        return True
+
+                return False
+
+            return backtrack(0, "")
+
+        return dfs(bottom)
+        
