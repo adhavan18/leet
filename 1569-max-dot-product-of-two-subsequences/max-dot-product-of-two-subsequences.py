32ba9c40acbca544991588fefeb56d968acb40ae
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-01-09 11:00:59 +0530

    Time: 115 ms (97.97%) | Memory: 22.1 MB (29.95%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
@@ -1,34 +1,34 @@
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        
-        memo = {}
+    # Bottom Up 
+        m = len(nums1)
+        n = len(nums2)
+        dp = [[0] * (n + 1) for _ in range (m + 1)]
+        for i in range (m + 1):
+            dp[i][n] = float('-inf')
+        for i in range (n + 1):
+            dp[m][i] = float('-inf')
+        for i in range (m - 1, -1, -1):
+            for j in range (n - 1, -1, -1):
+                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1],
+                               nums1[i] * nums2[j] + max(dp[i + 1][j + 1], 0)
+                            )
+        return dp[0][0]
 
-        # max dot product of two subsequences starting at i,j
-        def dp(i,j):
 
+    # Top Down 
+        cache = {}
+        def dfs(i, j):
             if i == len(nums1) or j == len(nums2):
-                return float("-inf") # if we are passed the boundary, dont pick anything from there.
-
-            if (i,j) in memo:
-                return memo[(i,j)]
-
-            take = nums1[i] * nums2[j]
-            res = max(
-            # take i,j. move forward
-                take + dp(i+1, j+1),
-
-            # take this subsequence and just end.
-                take,
-
-            # skip i: i+1,j
-                dp(i+1,j),
-
-            # skip j: i,j+1
-                dp(i,j+1),
-            )
-
-            memo[(i,j)] = res
-
-            return memo[(i,j)]
-
-        return dp(0,0)
+                return float('-inf')
+            if (i, j) in cache:
+                return cache[(i, j)]
+            skip_i = dfs(i + 1, j)
+            skip_j = dfs(i, j + 1)
+            not_skip = nums1[i] * nums2[j] + max(dfs(i + 1, j + 1), 0)
+            cache[(i, j)] = max(skip_i, skip_j, not_skip)
+            return cache[(i, j)]
+        return dfs(0, 0)
+            
+            
+                
