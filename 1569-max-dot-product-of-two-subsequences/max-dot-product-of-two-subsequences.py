c393fc56936a2c1992f6913787347261d6d32179
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-01-09 11:00:40 +0530

    Time: 589 ms (6.60%) | Memory: 109.2 MB (5.58%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
@@ -1,27 +1,34 @@
 class Solution:
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
-        n, m = len(nums1), len(nums2)
         
-        if m > n:
-            return self.maxDotProduct(nums2, nums1)
-            
-        dp = [float('-inf')] * (m + 1)
-        
-        for i in range(1, n + 1):
-            prev_diag = float('-inf')
-            
-            for j in range(1, m + 1):
-                curr_product = nums1[i-1] * nums2[j-1]
-                
-                temp = dp[j]
-                
-                dp[j] = max(
-                    curr_product,               
-                    curr_product + prev_diag,   
-                    dp[j],                       
-                    dp[j-1]                      
-                )
-                
-                prev_diag = temp
+        memo = {}
 
-        return dp[m]
+        # max dot product of two subsequences starting at i,j
+        def dp(i,j):
+
+            if i == len(nums1) or j == len(nums2):
+                return float("-inf") # if we are passed the boundary, dont pick anything from there.
+
+            if (i,j) in memo:
+                return memo[(i,j)]
+
+            take = nums1[i] * nums2[j]
+            res = max(
+            # take i,j. move forward
+                take + dp(i+1, j+1),
+
+            # take this subsequence and just end.
+                take,
+
+            # skip i: i+1,j
+                dp(i+1,j),
+
+            # skip j: i,j+1
+                dp(i,j+1),
+            )
+
+            memo[(i,j)] = res
+
+            return memo[(i,j)]
+
+        return dp(0,0)
