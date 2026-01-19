efcce7fec46c4cf86882983c7d45eb2404e28a54
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-01-19 19:22:42 +0530

    Time: 688 ms (30.29%), Space: 19.8 MB (13.14%) - LeetHub

--- 1895-largest-magic-square/1895-largest-magic-square.py
+++ 1895-largest-magic-square/1895-largest-magic-square.py
@@ -1,30 +1,35 @@
+from typing import List
+
 class Solution:
     def largestMagicSquare(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        res = 1
-
-        def isValid(i, j, k):
-            s = None
+        
+        row_sum = [[0] * (n + 1) for _ in range(m)]
+        col_sum = [[0] * n for _ in range(m + 1)]
+        
+        for i in range(m):
+            for j in range(n):
+                row_sum[i][j + 1] = row_sum[i][j] + grid[i][j]
+                col_sum[i + 1][j] = col_sum[i][j] + grid[i][j]
+        
+        def check(i, j, k):
+            target = row_sum[i][j + k] - row_sum[i][j]
             for x in range(i, i + k):
-                row = sum(grid[x][j:j + k])
-                if s is None: s = row
-                elif s != row: return False
-
-            for y in range(j, j + k):
-                if sum(grid[x][y] for x in range(i, i + k)) != s:
+                if row_sum[x][j + k] - row_sum[x][j] != target:
                     return False
-
-            if sum(grid[i + d][j + d] for d in range(k)) != s:
+            for y in range(j, j + k):
+                if col_sum[i + k][y] - col_sum[i][y] != target:
+                    return False
+            if sum(grid[i + d][j + d] for d in range(k)) != target:
                 return False
-
-            if sum(grid[i + d][j + k - 1 - d] for d in range(k)) != s:
+            if sum(grid[i + d][j + k - 1 - d] for d in range(k)) != target:
                 return False
-
             return True
-
+        
+        res = 1
         for k in range(2, min(m, n) + 1):
             for i in range(m - k + 1):
                 for j in range(n - k + 1):
-                    if isValid(i, j, k):
+                    if check(i, j, k):
                         res = k
         return res
