25b08c817399d902c3297facc3a0c9992aae42ff
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-04-15 21:50:43 +0530

    Time: 1 ms (17.74%) | Memory: 19.5 MB (20.97%) - LeetSync

--- 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
+++ 2598-shortest-distance-to-target-string-in-a-circular-array/shortest-distance-to-target-string-in-a-circular-array.py
@@ -5,6 +5,7 @@ class Solution:
 
         for i in range(n):
             if words[i] == target:
-                dist = abs(i -startIndex)
+                dist = abs(i - startIndex)
                 ans = min(ans, min(dist, n - dist))
+
         return ans if ans != float('inf') else -1
