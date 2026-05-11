2fc8349bbd76ad632b9295ac6514348f2c6e9199
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-05-11 10:09:32 +0530

    Time: 3 ms (76.03%), Space: 19.5 MB (15.91%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def separateDigits(self, nums: List[int]) -> List[int]:
         result = []
-        for num in nums:
-            s = str(num)
+        for i in nums:
+            s = str(i)
             for ch in s:
                 result.append(int(ch))
         return result
