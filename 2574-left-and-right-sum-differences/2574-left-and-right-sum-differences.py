71c6a3cf4ad1afe60cf1322c19e07a788413d402
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-06-06 10:27:55 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.97%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.py
@@ -2,11 +2,49 @@ from typing import List
 
 class Solution:
     def leftRightDifference(self, nums: List[int]) -> List[int]:
+        # Efficient O(n) Solution
+        totalSum = sum(nums)
+        leftSum = 0
         answer = []
 
-        for i in range(len(nums)):
-            leftSum = sum(nums[:i])
-            rightSum = sum(nums[i + 1:])
+        for num in nums:
+            rightSum = totalSum - leftSum - num
             answer.append(abs(leftSum - rightSum))
+            leftSum += num
 
         return answer
+
+
+        # --------------------------------------------------
+        # Brute Force O(n²) using slicing
+        # --------------------------------------------------
+        #
+        # answer = []
+        #
+        # for i in range(len(nums)):
+        #     leftSum = sum(nums[:i])
+        #     rightSum = sum(nums[i + 1:])
+        #     answer.append(abs(leftSum - rightSum))
+        #
+        # return answer
+
+
+        # --------------------------------------------------
+        # Brute Force O(n²) using nested loops
+        # --------------------------------------------------
+        #
+        # answer = []
+        #
+        # for i in range(len(nums)):
+        #     leftSum = 0
+        #     rightSum = 0
+        #
+        #     for j in range(len(nums)):
+        #         if j < i:
+        #             leftSum += nums[j]
+        #         elif j > i:
+        #             rightSum += nums[j]
+        #
+        #     answer.append(abs(leftSum - rightSum))
+        #
+        # return answer
