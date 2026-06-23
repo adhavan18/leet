068abe4aeb3bf0b81b3bde72ab1a2d97cf22f9b3
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-06-23 10:23:17 +0530

    Time: 3573 ms (92.11%), Space: 19.4 MB (86.84%) - LeetHub

---
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.py
@@ -0,0 +1,12 @@
+class Solution:
+    def zigZagArrays(self, n: int, l: int, r: int) -> int:
+        M = 10**9+7
+        m = r - l + 1
+        dp = [1] * m
+
+        for i in range(2, n + 1):
+            dp.reverse()
+            s = 0
+            for j in range(m):
+                dp[j], s = s, (s + dp[j]) % M
+        return (sum(dp) % M << 1) % M
