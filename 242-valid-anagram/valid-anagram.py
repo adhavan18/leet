c595d486401f71c454a9473a67a31547407fe6b7
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-06-28 10:30:08 +0530

    Time: 15 ms (44.22%) | Memory: 20.5 MB (9.73%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,19 +1,3 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
-        
-#from collections import Counter
-#class Solution:
-#    def isAnagram(self, s: str, t: str) -> bool:
-#        return Counter(s) == Counter(t)      
-
-#class Solution:
-#    def isAnagram(self, s: str, t: str) -> bool:
-#       if len(s) != len(t):
-#           return False
-#       count = {}
-#       for ch in s:
-#           count[ch] = count.get(ch, 0) + 1
-#       for ch in t:
-#           count[ch] = count.get(ch, 0) - 1
-#       return all(v == 0 for v in count.vues())
