38b041bc1de64b290e1c34709b21b35f1bbc4436
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-07-22 17:16:04 +0530

    Time: 635 ms (67.27%), Space: 21 MB (61.82%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.py
@@ -10,16 +10,13 @@ class Solution:
 
             while i < n and s[i] == s[start]:
                 i += 1
-
             if s[start] == "0":
                 zeroBlocks.append(i - start)
-
         m = len(zeroBlocks)
-
         if m < 2:
             return cnt1
-
-        bestGain = 0  # Optimal Increment
+        bestGain = 0
         for i in range(m - 1):
             bestGain = max(bestGain, zeroBlocks[i] + zeroBlocks[i + 1])
         return cnt1 + bestGain
+        
