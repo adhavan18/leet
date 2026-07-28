2b85245aed4a85b8069e4c11a928822d40cd3c21
Tamil Adhavan S K <99407456+adhavan18@users.noreply.github.com>
2026-07-28 10:27:07 +0530

    Time: 271 ms (50.69%), Space: 21 MB (39.17%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -1,5 +1,5 @@
 class Solution:
-    def smallestPalindrome(self, s: str) -> str:
+    def smallestPalindrome(self, s: str):
         freq = {}
         for ch in s:
             freq[ch] = freq.get(ch, 0) + 1
@@ -7,12 +7,7 @@ class Solution:
         middle = ""
         for ch in sorted(freq.keys()):
             left += ch * (freq[ch] // 2)
-
             if freq[ch] % 2 == 1:
                 middle = ch
         ans = left + middle + left[::-1]
-
         return ans
-
-
-        
